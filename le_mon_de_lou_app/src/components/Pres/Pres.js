5e69db5d5e6d83d9c50063edd8ed4c9b7ed69f9b
lou Wargnier <louis.bienvenue-wargnier@epitech.eu>
2023-11-14 10:39:02 +0100

    [ADD] description and title

---
+++ le_mon_de_lou_app/src/components/Pres/Pres.js
@@ -0,0 +1,15 @@
+import './Pres.css'
+
+function Pres() {
+    return (
+        <div className='div_pres'>
+            <div className='div_content'>
+                <h2 className='h2_pres'>Pika_louu</h2>
+                <p>20 ans</p>
+                <p>Passionné de Pokemon, de Genshin et développeur de jeux vidéo a mes heures perdu, voici un aperçu de mon univers.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Pres
--- le_mon_de_lou_app/src/pages/Accueil.js
+++ le_mon_de_lou_app/src/pages/Accueil.js
@@ -1,9 +1,12 @@
 import CardList from '../components/CardList/CardList';
+import Pres from '../components/Pres/Pres';
 import './Accueil.css'
 
 function Accueil() {
     return (
         <div className='accueil'>
+            <Pres></Pres>
+            <h2>Mes univers</h2>
             <CardList>
             </CardList>
         </div>
